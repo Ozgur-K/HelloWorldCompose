f6ce3a9715a06c08aea92e1f6383c577f89b51b7
Ozgur <evrenater@gmail.com>
2025-05-12 19:51:25 +0300

    some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.helloworldcompose"
-    compileSdk = 35
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.helloworldcompose"
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
