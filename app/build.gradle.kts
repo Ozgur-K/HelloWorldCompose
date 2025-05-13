4050f80b6f45d0bd92db216ae6fbac4d13809c36
Ozgur <evrenater@gmail.com>
2025-05-13 19:29:27 +0300

    gradle again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
-    id("com.android.application")
+    id("com.android.application") version "8.10.0" apply false
+    id("com.android.library") version "8.10.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
     kotlin("android")
 }
+
 
 android {
     namespace = "com.example.helloworldcompose"
